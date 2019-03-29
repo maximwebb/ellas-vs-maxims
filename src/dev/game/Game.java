de5d535c63586d69ddc567efb9c8a8b4d8ca06de
Maxim Webb <maximwebb@gmail.com>
2019-03-29 22:37:18 +0000

    Remove junk

--- src/dev/game/Game.java
+++ src/dev/game/Game.java
@@ -10,12 +10,6 @@ public class Game implements Runnable {
 	private Display display;
 	public int width, height;
 	public String title;
-	private int xCoord = 0;
-	private int yCoord = 0;
-	private int xVel = 5;
-	private int yVel = 5;
-	private int rectWidth = 200;
-	private int rectHeight = 200;
 
 	private boolean running = false;
 	private Thread thread;
@@ -34,16 +28,9 @@ public class Game implements Runnable {
 		display = new Display(title, width, height);
 	}
 
+	/* Updates to various objects happen here */
 	private void tick() {
-		xCoord += xVel;
-		yCoord += yVel;
 
-		if ((xCoord + rectWidth > width) || xCoord < 0) {
-			xVel = -xVel;
-		}
-		if ((yCoord + rectHeight > height) || yCoord < 0) {
-			yVel = -yVel;
-		}
 	}
 
 	private void render() {
@@ -56,7 +43,7 @@ public class Game implements Runnable {
 		/* Draw graphics */
 		g.clearRect(0, 0, width, height);
 		g.setColor(Color.CYAN);
-		g.fillRect(xCoord, yCoord, rectWidth, rectHeight);
+		g.fillRect(0, 0, 100, 100);
 
 		bs.show();
 		g.dispose();
