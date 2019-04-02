d290a2aaa6974fa42d1d620b7c4fed6f8843a99f
BananaGrandmaster <48969720+BananaGrandmaster@users.noreply.github.com>
2019-04-02 20:48:26 +0100

    add vectors

--- src/dev/game/plants/EggFlower.java
+++ src/dev/game/plants/EggFlower.java
@@ -2,8 +2,8 @@ package dev.game.plants;
 
 import dev.game.*;
 import dev.game.gfx.Assets;
-import dev.game.rooms.GameRoom;
-import dev.game.rooms.Room;
+import dev.game.rooms.*;
+import dev.game.maths.Vector2D;
 
 public class EggFlower extends Plant {
 
